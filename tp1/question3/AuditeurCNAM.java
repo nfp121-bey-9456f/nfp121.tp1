69b2a7d7d92876f513ae4f75b68dfc4831014e0d
Elie DAHER <elie.daher@isae.edu.lb>
2019-03-11 09:13:42 +0200

    Apres ma reponse sur la troisieme question

--- tp1/question3/AuditeurCNAM.java
+++ tp1/question3/AuditeurCNAM.java
@@ -3,10 +3,11 @@ package question3;
 /**
  * NFP121 TpIntroduction, usage de BlueJ et du "Submitter".
  * 
- * @version septembre 2009
- * @author à compléter
+ * @version Mars 2019
+ * @author Elie DAHER
  * @see java.lang.String, java.lang.Math
  */
+
 public class AuditeurCNAM {
     /** l'attribut nom de chaque auditeur. */
     private String nom;
@@ -14,6 +15,8 @@ public class AuditeurCNAM {
     private String prenom;
     /** l'attribut matricule de chaque auditeur. */
     private String matricule;
+    /** l'attribut login de chaque auditeur. */
+    private String login;
 
     /**
      * "Création", le constructeur d'un auditeur avec son nom, son prénom et son
@@ -45,7 +48,57 @@ public class AuditeurCNAM {
      *         homonymes...
      */
     public String login() {
-        return "";// à compléter
+        String n = nom;
+        String p = prenom;
+
+        /**
+         * Pas de caractères accentués pour le login
+         * Voici une Méthode pour soumettre un String avec accents enlever les accents
+         * @return String sans accents 
+         */
+        n = pasAccents(n);
+        p = pasAccents(p);
+
+        /**
+         * Le trait d'union, ou spéciaux <i>(pour unix)</i> sont remplacés par des '_' 
+         */
+        n = n.replaceAll("[^a-zA-Z0-9]+","_");
+        p = p.replaceAll("[^a-zA-Z0-9]+","_");
+
+        /**
+         * Tous les caractères sont convertis en minuscules
+         */
+        String nm = n.toLowerCase();
+        String pm = p.toLowerCase();
+
+        /** 
+         * Le login est composé des 6 premieres lettres du nom suivies de la première
+         * lettre du prénom séparées de '_' 
+         * 
+         * Utilisation de Math.min pour eviter StringIndexOutOfBoundsException 
+         */
+
+        login = nm.substring(0,Math.min(nm.length(),6)) + "_" + pm.substring(0,1);
+
+        return login;
+    }
+
+    /**
+     * Méthode pour enlever les accents
+     * Soumettre un String avec accents
+     * Retourner le String sans accents
+     */
+    public String pasAccents(String x){
+        String accents = "àâäãéèêëîïìöôòõüûùÀÂÄÃÉÈÊËÎÏÌÖÔÒÕÜÛÙ";
+        //tableau regroupant les accents
+        String paccents = "aaaaeeeeiiioooouuuaaaaeeeeiiioooouuu";
+        //tableau ayant les mêmes lettres mais sans accents
+        int l = accents.length();
+        //longueur du tableau
+        for(int i = 0; i<l ; i++){
+            x = x.replace(accents.charAt(i), paccents.charAt(i));
+        }
+        return x;
     }
 
     /**
@@ -54,7 +107,7 @@ public class AuditeurCNAM {
      * @return son nom
      */
     public String nom() {
-        return null;// à compléter
+        return this.nom;        
     }
 
     /**
@@ -63,7 +116,7 @@ public class AuditeurCNAM {
      * @return son prénom
      */
     public String prenom() {
-        return null;// à compléter
+        return this.prenom;
     }
 
     /**
@@ -72,7 +125,7 @@ public class AuditeurCNAM {
      * @return son matricule
      */
     public String matricule() {
-        return null;// à compléter
+        return this.matricule;
     }
 
     /**
